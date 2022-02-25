a76137792a8a0dd8c222f3e96f4d37c8effbc3d7
Jit Mondal <97719835+JitaDeveloper@users.noreply.github.com>
2022-02-25 15:32:22 +0530

    Add files via upload

---
+++ 52_pr_adding elements to tuple.py
@@ -0,0 +1,13 @@
+# creating a tuple
+t=(2.3,3,True,'Jit')
+print(t)
+
+#converting the tuple into a list
+t=list(t)
+
+#adding  one element in the list
+t.append("Mondal")
+
+#converting the list into further tuple
+t=tuple(t)
+print(t)
